e5049e22c0ffe4626d6d37b082dc6fa3bec31777
davidlee <david@davelee.com.au>
2009-10-12 11:24:55 +1100

    Proposed patches:  - change argument in Jquery.fn.swap so var b doesn't conflict with fn argument b  - add semicolons for consistency (no effect)  - fallthrough in case on backspace so backspace widens search results  - enforce maxEntries when defaults are provided  - in buildItemsList, append for each element; fix this -> item  - add callbacks for adding / removing item

--- jquery.listface.js
+++ jquery.listface.js
@@ -2,8 +2,8 @@
 
   // Utility methods
   jQuery.fn.extend({
-    swap: function(b) {
-      var b = jQuery(b)[0];
+    swap: function(other) {
+      var b = jQuery(other)[0];
       var a = this[0];
       var a2 = a.cloneNode(true);
       var b2 = b.cloneNode(true);
@@ -11,14 +11,14 @@
       a.parentNode.replaceChild(b2, a);
       b.parentNode.replaceChild(a2, b);
       stack[0] = a2;
-      return this.pushStack(stack)
+      return this.pushStack(stack);
     },
   })
 
   // $.listface()
   $.listface = function(selector, options) {
     if (!$(selector).length) throw "Couldn't find a text field with the ID specified";
-    
+
     $.extend($.listface, {
       reset: function(selector) { reset(selector) }
     })
@@ -41,9 +41,9 @@
   	var mapping = [];
 
     verify(options);
-    init(selector, options)  	
-  	preloadDefaults()
-	
+    init(selector, options);
+  	preloadDefaults();
+
   	function init(selector, opts) {
   	  originalTextField = $(selector);
   	  options = opts;
@@ -51,9 +51,9 @@
   	  form.submit(function() { list.remove(); originalTextField.show() })
   	  if (opts.attribute) usesObject = true;
       replaceWithList();
-      loadAutoComplete()
+      loadAutoComplete();
   	}
-  	
+
     // Replaces the text field with the necessary list element that
     // will contain the results, etc.
     function replaceWithList() {
@@ -71,10 +71,10 @@
       textField = $('#listface-' + originalTextField.attr('id') + ' :input');
       items = $('#listface-' + originalTextField.attr('id') + ' ul.items');
     }
-    
+
     function reset(selector) {
       $(selector).prev('ul.listface').remove();
-      $.listface(selector, $(selector).data('listfaceopts'))
+      $.listface(selector, $(selector).data('listfaceopts'));
     }
 
     function loadAutoComplete() {
@@ -84,7 +84,7 @@
             .remove()
             .end()
           .append('<li class="hint">' + (options.hint ? options.hint : 'Start typing...') + '</li>')
-          .show('slow')
+          .show('slow');
       })
       textField.blur(function() { setTimeout(function() { clearFocus(); items.hide('slow', function() { $(this).empty() }) }, 100) })
       textField.keydown(function(event) {
@@ -120,7 +120,7 @@
                 setFocus(list.find('.selected:last'));
               }
             }
-            break;
+            // fall through (backspace widens search)
           default:
             clearFocus();
             clearTimeout(timeout);
@@ -135,21 +135,34 @@
       items.show('slow');
       var url;
       if (options.param) {
-        url = options.url + ['?', options.param, '=', textField.val().toLowerCase()].join('')
+        url = options.url + ['?', options.param, '=', textField.val().toLowerCase()].join('');
       } else {
-        url = options.url
+        url = options.url;
       }
       $.getJSON(url, function(results) {
-        buildItemsList(excludeExistingEntries(results))
+        buildItemsList(excludeExistingEntries(results));
       })
     }
 
     function excludeExistingEntries(results) {
       results = $.grep(results, function(result) {
         var id = (usesObject ? result[options.attribute.value] : result);
-        return $.inArray(id.toString(), $.map(mapping, function(item) { return $(item.find('span')[ (usesObject ? 1 : 0) ]).text() })) < 0
+        return $.inArray(id.toString(), $.map(mapping, function(item) { return $(item.find('span')[ (usesObject ? 1 : 0) ]).text() })) < 0;
       })
       return results;
+    };
+
+    function enforceMaxEntries() {
+      var activeEntryCount = list.find('li.selected').length;
+      if (options.maxEntries) {
+        if (activeEntryCount >= options.maxEntries) {
+          textField.addClass('disabled');
+          textField.attr('disabled', 'disabled');
+        } else {
+          textField.removeClass('disabled');
+          textField.removeAttr('disabled');
+        }
+      }
     }
 
     // Adds an item to the "selected" items list
@@ -161,26 +174,30 @@
       item.find('a').click(function(event) { event.preventDefault(); remove(item) });
       list.find('.listface-input').before(item.addClass('selected'));
       textField.val('');
-      if (options.maxEntries && list.find('li.selected').length >= options.maxEntries) {
-        textField.addClass('disabled');
-        textField.attr('disabled', 'disabled')
+      enforceMaxEntries();
+      if(options.addItem) {
+        var name = item.find('span:first').text();
+        var id   = item.find('span:last').text();
+        options.addItem(item, id, name);
       }
     }
 
     function remove(item) {
       mapping = $.grep(mapping, function(i) { return i[0] != item[0] });
       item.remove();
-      syncMapping()
-      if (options.maxEntries && list.find('li.selected').length < options.maxEntries) {
-        textField.removeClass('disabled');
-        textField.removeAttr('disabled')
+      syncMapping();
+      enforceMaxEntries();
+      if(options.removeItem) {
+        var name = item.find('span:first').text();
+        var id   = item.find('span:last').text();
+        options.removeItem(item, id, name);
       }
     }
 
     // Syncs what's in the mapping variable with the values in the originalTextField
     function syncMapping() {
       var values = $.map(mapping, function(item) { return $(item.find('span')[ (usesObject ? 1 : 0) ]).text() }).join(', ');
-      originalTextField.val(values)
+      originalTextField.val(values);
     }
 
     // Executed when the user presses the up arrow
@@ -190,7 +207,7 @@
         clearFocus();
         return false;
       } else {
-        setFocus(items.focused.prev())
+        setFocus(items.focused.prev());
       }
     }
 
@@ -209,57 +226,57 @@
     function setFocus(item) {
       if (items.focused) items.focused.removeClass('focused');
       $(item).addClass('focused');
-      items.focused = item
+      items.focused = item;
     }
 
     // Clears the current focus
     function clearFocus() {
       items.focused = undefined;
       items.find('li').removeClass('focused');
-      list.find('li').removeClass('focused')
+      list.find('li').removeClass('focused');
     }
 
-    function buildItemsList(data) {
-      items.empty();
-      var li;
-      if ($(data).length) {
-        $(data).each(function() {
-          if (usesObject) {
-            li = $([
-              '<li>',
-                '<span>', this[options.attribute.name], '</span>',
-                '<span style="display: none">', this[options.attribute.value], '</span>',
-              '</li>'
-            ].join(''))
+      function buildItemsList(data) {
+        items.empty();
+        var li;
+        if ($(data).length) {
+          $(data).each(function() {
+            if (usesObject) {
+              li = $([
+                '<li>',
+                  '<span>', this[options.attribute.name], '</span>',
+                  '<span style="display: none">', this[options.attribute.value], '</span>',
+                '</li>'].join(''));
+            } else {
+              li = $('<li><span>' + this + '</span></li>');
+            }
+            li.hover(function() { $(this).addClass('focused') }, function() { $(this).removeClass('focused') } );
+            li.click(function() { add($(this)) });
+            items.append(li);
+          });
+        } else {
+          li = $('<li class="no_results">' + (options.no_results ? options.no_results : 'No results found') + '</li>');
+          items.append(li);
+        }
+      }
+
+      // loads a string or an object as a list item
+      function load(item) {
+          // expect object if we're talking objects here
+          var li;
+          if (options.attribute) {
+              li = $([
+                '<li>',
+                  '<span>', item[options.attribute.name], '</span>',
+                  '<span style="display: none">', item[options.attribute.value], '</span>',
+                '</li>'
+              ].join(''));
           } else {
-            li = $('<li><span>' + this + '</span></li>');
+              li = $('<li><span>' + item + '</span></li>');
           }
-          li.hover(function() { $(this).addClass('focused') }, function() { $(this).removeClass('focused') } );
-          li.click(function() { add($(this)) } )
-        })
-      } else {
-        li = $('<li class="no_results">' + (options.no_results ? options.no_results : 'No results found') + '</li>');
+          window.lo = li;
+          add(li);
       }
-      items.append(li)
-    }
-
-    // loads a string or an object as a list item
-    function load(item) {
-      // expect object if we're talking objects here
-      var li;
-      if (options.attribute) {
-        li = $([
-          '<li>',
-            '<span>', item[options.attribute.name], '</span>',
-            '<span style="display: none">', this[options.attribute.value], '</span>',
-          '</li>'
-        ].join(''))
-      } else {
-        li = $('<li></span>' + item + '</span></li>')
-      }
-      window.lo = li;
-      add(li);
-    }
 
     function preloadDefaults() {
       if (!options.defaults) return false;
@@ -276,6 +293,8 @@
       if (options.defaults && options.defaults.constructor != Array)          { throw "defaults needs to be an array" }
       if (options.attribute &&
         (!("value" in options.attribute) || !("name" in options.attribute)))  { throw "attribute needs to have value and name" }
+      if (options.addItem && options.addItem.constructor != Function)         { throw "addItem needs to be a Function" }
+      if (options.removeItem && options.removeItem.constructor != Function)   { throw "addItem needs to be a Function" }
       // add further validation here
     }
 }
